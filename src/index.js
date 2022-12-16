693da5dac9fefdbd483dc4bb819457f957a62095
eltatataa <davidtowo75@gmail.com>
2022-12-16 18:42:51 -0500

    messages & YT_API search

--- src/index.js
+++ src/index.js
@@ -3,7 +3,14 @@ dotenv.config();
 const { Client, GatewayIntentBits } = require('discord.js');
 const { google } = require('googleapis');
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent
+	],
+});
+
 const youtube = google.youtube({ version: 'v3', auth: process.env.API_KEY });
 
 client.on('ready', () => {
@@ -29,27 +36,26 @@ client.on('interactionCreate', async interaction => {
 	if (interaction.commandName === 'ping') {
 		await interaction.reply('Pong!');
 	}
+});
 
-	if (interaction.commandName === 'yt') {
+client.on('messageCreate', message => {
+	if (message.content.startsWith('!youtube')) {
+		const search = message.content.split(' ').slice(1).join(' ');
+
 		youtube.search.list({
 			part: 'id,snippet',
 			type: 'video',
-			q: 'despacito'
+			q: search
 		}, async (err, res) => {
 			if (err) return console.error(err);
 
 			// aquí puedes procesar los resultados de la búsqueda
 			const video = res.data.items[0];
-			const videoTitle = video.snippet.title;
 			const videoUrl = `https://www.youtube.com/watch?v=${video.id.videoId}`;
 
-			await interaction.reply(videoUrl);
+			await message.reply(videoUrl);
 		});
 	}
-});
-
-client.on('message', message => {
-	console.log(message);
 });
 
 client.login(process.env.TOKEN);
